96888e7d137ae29853e8a3b3d860e7f5c4fad0c9
vaibhav gurnani <macmini5@MacMini5s-Mac-mini.local>
2020-12-22 19:40:00 +0530

    feat: Add investor and company implementation

--- src/components/Companies/index.js
+++ src/components/Companies/index.js
@@ -2,6 +2,10 @@ import React , { useState ,useRef }from 'react'
 import { withRouter } from "react-router";
 import CompaniesList from './list'
 import Pagination from "react-js-pagination";
+import { ToastContainer } from "react-toastr";
+import { gql, useMutation } from '@apollo/client';
+import Modal from 'react-bootstrap-modal';
+
 // import { useQuery, gql } from '@apollo/client';
 
 
@@ -12,14 +16,24 @@ import Pagination from "react-js-pagination";
 //       }
 //   }
 // `;
-
+const ADD_COMPANY = gql`
+  mutation AddCompany($name: String!) {
+    insert_company_one(object: {name: $name}) {
+      id
+      company_id
+      amount
+    }
+  }
+`;
 const Companies = (props) =>{
   const [activePage, setActivePage ] = useState(1)
   const [searchFilter, setSearchFilter ] = useState('')
-
+  const [formData , setFormData ] = useState({})
+  const [openModal, setOpenModal] = useState(false)
   const [offset, setOffset ] = useState(0);
   const search_filter = useRef();
-
+  const [insert_company_one, addCompany ] = useMutation(ADD_COMPANY);
+  const toastr = useRef();
   const limit = 10;
   //const {loading, data } = useQuery(GET_INVESTORS);
 
@@ -33,10 +47,39 @@ const Companies = (props) =>{
   const handleFilter = (event) => {
     setSearchFilter(search_filter.current.value)
   }
+
+  const openModalFun = () => {
+    setOpenModal(true)
+}
+  const closeModalFun = () => {
+      setOpenModal(false)
+  }
+  const handleChange = (event) => {
+    formData[event.target.name] = event.target.value;
+    setFormData(formData)
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    insert_company_one({name: formData.name}).then((data)=>{
+      closeModalFun()
+      toastr.current.success('Successfully Created','Success')
+      props.history.push(`/`)
+      props.history.go();
+    }).catch((error)=>{
+    })
+  }
   return(
     <div >
-        <input type='text' ref={search_filter} name='searchFilter'  />
-        <button onClick={handleFilter} >search</button>
+        <ToastContainer
+          className="toast-top-right"
+          ref={toastr}
+        />
+      <div >
+       <h4>Companies</h4><button onClick={openModalFun}>Add company</button>
+       <input type='text' ref={search_filter} name='searchFilter'  />
+       <button onClick={handleFilter} >search</button>
+       </div>
         <CompaniesList limit={ limit } offset={ offset } searchFilter={searchFilter}  />
         {
           searchFilter !=='' ? null : <Pagination
@@ -47,6 +90,32 @@ const Companies = (props) =>{
           onChange={handlePageChange}
           /> 
         }
+
+<Modal
+        show={openModal}
+        onHide={closeModalFun}
+      >
+          <Modal.Header closeButton>
+          <Modal.Title></Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+            
+          <span>{addCompany.error?.message}</span>
+          <div>
+              <h4>Add Company</h4>
+              <h4>Please enter details of the company</h4>
+              <form onSubmit={handleSubmit}>
+                  <input type='text' name='name' onChange={(event) => handleChange(event)} placeholder='name' /><br/>
+                  <input type='button' onClick={closeModalFun} value='cancel'/>
+                  <input type='submit' value='submit'/>
+              </form>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+        </Modal.Footer>
+
+
+      </Modal>
     </div>
 
   )
--- src/components/Investors/index.js
+++ src/components/Investors/index.js
@@ -2,7 +2,9 @@ import React , { useState ,useRef }from 'react'
 import { withRouter } from "react-router";
 import InvestorsList from './list'
 import Pagination from "react-js-pagination";
-// import { useQuery, gql } from '@apollo/client';
+import Modal from 'react-bootstrap-modal';
+import { ToastContainer } from "react-toastr";
+import { gql, useMutation } from '@apollo/client';
 
 
 // const GET_INVESTORS = gql`
@@ -12,12 +14,27 @@ import Pagination from "react-js-pagination";
 //       }
 //   }
 // `;
+const ADD_INVESTOR = gql`
+  mutation AddInvestor($name: String!, $photo_large: String!, $photo_thumbnail: String!) {
+    insert_investor_one(object: {name: $name,amount:  $photo_large, photo_thumbnail: $photo_thumbnail}) {
+      id
+      company_id
+      amount
+    }
+  }
+`;
 
 const Investors = (props) =>{
   const [activePage, setActivePage ] = useState(1)
+  const [openModal, setOpenModal] = useState(false)
   const [searchFilter, setSearchFilter ] = useState('')
+  const [formData , setFormData ] = useState({})
+  const [insert_investor_one, addInvestor ] = useMutation(ADD_INVESTOR);
+
   const [offset, setOffset ] = useState(0);
   const limit = 10;
+  const toastr = useRef();
+
   const search_filter = useRef();
 
   //const {loading, data } = useQuery(GET_INVESTORS);
@@ -29,14 +46,42 @@ const Investors = (props) =>{
     const offest = page === '1' ? 0 : parseInt((page-1)+"1")
     setOffset(offest)
   }
-
+  const openModalFun = () => {
+    setOpenModal(true)
+}
+  const closeModalFun = () => {
+      setOpenModal(false)
+  }
   const handleFilter = (event) => {
     setSearchFilter(search_filter.current.value)
   }
+  const handleChange = (event) => {
+    formData[event.target.name] = event.target.value;
+    setFormData(formData)
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    insert_investor_one({name: formData.name, photo_large: formData.photo_large, photo_thumbnail: formData.photo_thumbnail}).then((data)=>{
+      closeModalFun()
+      toastr.current.success('Successfully Created','Success')
+      props.history.push(`/`)
+      props.history.go();
+    }).catch((error)=>{
+    })
+  }
   return(
     <div >
+       <ToastContainer
+          className="toast-top-right"
+          ref={toastr}
+        />
+        <div >
+       
+        <h4>Investors</h4><button onClick={openModalFun}>Add investor</button>
         <input type='text' ref={search_filter} name='searchFilter'  />
         <button onClick={handleFilter} >search</button>
+        </div>
         <InvestorsList limit={ limit } offset={ offset } searchFilter={searchFilter} />
         {
           searchFilter !=='' ? null : <Pagination
@@ -47,6 +92,35 @@ const Investors = (props) =>{
           onChange={handlePageChange}
         /> 
       }
+
+    <Modal
+        show={openModal}
+        onHide={closeModalFun}
+      >
+          <Modal.Header closeButton>
+          <Modal.Title></Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+            
+          <span>{addInvestor.error?.message}</span>
+          <div>
+              <h4>Add Investor</h4>
+              <h4>Please enter details of the investor</h4>
+              <form onSubmit={handleSubmit}>
+                  <input type='text' name='name' onChange={(event) => handleChange(event)} placeholder='name' /><br/>
+                  <input type='text' name='photo_large' onChange={(event) => handleChange(event)} placeholder='photo_large' /><br/>
+                  <input type='text' name='photo_thumbnail' onChange={(event) => handleChange(event)} placeholder='photo_thumbnail'  /><br/>
+
+                  <input type='button' onClick={closeModalFun} value='cancel'/>
+                  <input type='submit' value='submit'/>
+              </form>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+        </Modal.Footer>
+
+
+      </Modal>
     </div>
 
   )
